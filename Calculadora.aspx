6be67cc7206d79551b1330eb60330a0e7bd00906
Paulo Conter <conterdev@gmail.com>
2023-11-06 13:12:23 -0300

    Add files via upload

---
+++ Calculadora.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Calculadora._Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>
+        Calculadora
+    </title>
+</head>
+<body>
+    <form id="form1" runat="server">
+            <h2>CALCULADORA</h2>
+            <div>
+                <label for="txtNumero1" aria-atomic="True">1° Número:</label>
+                <asp:TextBox ID="txtNumero1" runat="server" Width="44px"></asp:TextBox>
+                <br />
+
+                <label for="txtNumero2" aria-atomic="True">2° Número:</label>
+                <asp:TextBox ID="txtNumero2" runat="server" Width="44px"></asp:TextBox>
+                <br />
+
+                <label for="ddlOperacao">Operação:</label>
+                <asp:DropDownList ID="ddlOperacao" runat="server">
+                    <asp:ListItem Text="Somar" Value="soma" />
+                    <asp:ListItem Text="Subtrair" Value="subtracao" />
+                    <asp:ListItem Text="Multiplicar" Value="multiplicacao" />
+                    <asp:ListItem Text="Dividir" Value="divisao" />
+                </asp:DropDownList>
+                <br />
+
+                <asp:Button ID="btnCalcular" runat="server" Text="Calcular" OnClick="btnCalcular_Click" BackColor="#009933" ForeColor="White" />
+            </div>
+            <br />
+            <asp:Label ID="lblResultado" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Calculadora.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Calculadora
+{
+    public partial class _Default : Page
+    {
+        protected void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+            if (double.TryParse(txtNumero1.Text, out num1) && double.TryParse(txtNumero2.Text, out num2))
+            {
+                string operacao = ddlOperacao.SelectedValue;
+                double resultado = 0;
+
+                switch (operacao)
+                {
+                    case "soma":
+                        resultado = num1 + num2;
+                        break;
+                    case "subtracao":
+                        resultado = num1 - num2;
+                        break;
+                    case "multiplicacao":
+                        resultado = num1 * num2;
+                        break;
+                    case "divisao":
+                        if (num2 != 0)
+                        {
+                            resultado = num1 / num2;
+                        }
+                        else
+                        {
+                            lblResultado.Text = "Erro: Divisão por zero.";
+                            return;
+                        }
+                        break;
+                    default:
+                        lblResultado.Text = "Operação inválida.";
+                        return;
+                }
+
+                lblResultado.Text = "Resultado: " + resultado;
+            }
+            else
+            {
+                lblResultado.Text = "Por favor, insira números válidos.";
+            }
+        }
+    }
+}
